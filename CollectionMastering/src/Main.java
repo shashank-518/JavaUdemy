3007174706f46aedd0928a42645353c8800a3d37
Shashank S <shashank5418shashu@gmail.com>
2025-03-02 16:11:40 +0530

    Camparator learnt

--- CollectionMastering/src/Main.java
+++ CollectionMastering/src/Main.java
@@ -1,5 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Student{
+    private String name;
+
+    private double cgpa;
+
+    public Student(String name, double cgpa) {
+        this.name = name;
+        this.cgpa = cgpa;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCgpa() {
+        return cgpa;
+    }
+}
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Shashank" , 7.4));
+        students.add(new Student("Shreyas" , 7.9));
+        students.add(new Student("Vishwas" , 8.4));
+        students.add(new Student("Aditya" , 9.4));
+
+
+        students.sort(((o1, o2) -> (int) (o2.getCgpa() - o1.getCgpa())));
+
+        for(Student s : students){
+            System.out.println(s.getName() + "  " + s.getCgpa());
+        }
+
     }
 }
