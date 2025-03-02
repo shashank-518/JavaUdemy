011ad77f6393942d819caad837587ee7ed0c9e47
Shashank S <shashank5418shashu@gmail.com>
2025-03-02 22:41:35 +0530

    CopyOnWriteArrayList Learnt

---
+++ CollectionMastering/src/CopyOnWriteArray/CopyOnWriteArrayDemo.java
@@ -0,0 +1,52 @@
+package CopyOnWriteArray;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class CopyOnWriteArrayDemo {
+
+    public static void main(String[] args) {
+        CopyOnWriteArrayList<Integer> CopyArray = new CopyOnWriteArrayList<>();
+
+        CopyArray.add(24);
+        CopyArray.add(45);
+        CopyArray.add(67);
+        CopyArray.add(23);
+
+        for (Integer i : CopyArray){
+
+            if(i == 67){
+                CopyArray.add(56);
+            }
+            System.out.println(i);
+        }
+
+        System.out.println("Updated Array List is " + CopyArray);
+
+    }
+
+}
+
+/*
+
+It is similar to array List Only difference is that it when we are writing a copy of the array list is made.
+
+It is first updated in the copy Then to that original Array List
+
+It is not possible in normal Array List
+
+In Normal Array List If you are trying to fetch the element and in between if you try to insert
+this will throw you a concurrence error or exception
+
+
+for (Integer i : CopyArray){
+
+            if(i == 67){
+                CopyArray.add(56);
+            }
+            System.out.println(i);
+        }
+
+ If you try this using Array List this code will throw an error
+
+
+ */
