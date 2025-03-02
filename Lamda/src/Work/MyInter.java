30c488dfd08081cc9f9e602f9bf343c2c9552464
Shashank S <shashank5418shashu@gmail.com>
2025-03-02 10:08:53 +0530

    Learning about lambda Expression

---
+++ Lamda/src/Work/MyInter.java
@@ -0,0 +1,12 @@
+package Work;
+
+@FunctionalInterface
+public interface MyInter {
+
+    public abstract void sayHello();
+
+
+}
+
+//to use lamda expression we should have an interface with only one single abstract method
+// It is basically arrow function in javaScript
