57593985e21a8f52a29e5f350c5e6ff537a5739f
Shashank S <shashank5418shashu@gmail.com>
2025-03-02 16:25:25 +0530

    Manually createad a linked list

---
+++ CollectionMastering/src/LinkedList/Test.java
@@ -0,0 +1,29 @@
+package LinkedList;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+//        Node node1 = new Node();
+//        Node node2 = new Node();
+//
+//        node1.value = 1;
+//        node1.next = node2;
+//        node2.value = 2;
+//        node2.next = null;
+
+
+    }
+
+}
+
+
+//No necessary of doing this as java as special class called Linked List
+
+//class Node{
+//
+//    public int value;
+//
+//    public Node next;
+//
+//}
