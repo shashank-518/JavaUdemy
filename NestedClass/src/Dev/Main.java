1e4a3721771746e0b5d92222ee5665e8a8b3a9ce
Shashank S <shashank5418shashu@gmail.com>
2025-03-02 09:08:55 +0530

    Compartor function was used to sort them

--- NestedClass/src/Dev/Main.java
+++ NestedClass/src/Dev/Main.java
@@ -1,7 +1,26 @@
 package Dev;
 
+import Dev.domain.Employee;
+import Dev.domain.EmployeeComparator;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        List<Employee> employee = new ArrayList<>(List.of(
+                new Employee(101,"Shashank",2025),
+                new Employee(102,"Shreyas",2026),
+                new Employee(103,"Mizba",2025),
+                new Employee(104,"Aditya",2025)
+        )
+        );
+
+        var compartor = new EmployeeComparator<>();
+        employee.sort(compartor);
+
+        for(Employee e: employee){
+            System.out.println(e);
+        }
     }
 }
---
+++ NestedClass/src/Dev/domain/EmployeeComparator.java
@@ -0,0 +1,10 @@
+package Dev.domain;
+
+import java.util.Comparator;
+
+public class EmployeeComparator <T extends  Employee> implements Comparator<Employee> {
+    @Override
+    public int compare(Employee o1, Employee o2) {
+        return o1.getName().compareTo(o2.getName());
+    }
+}
