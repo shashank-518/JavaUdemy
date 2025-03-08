f49f5c0c9e1c74630e0c6c812828e4f7e57c43f0
Shashank S <shashank5418shashu@gmail.com>
2025-03-09 02:00:07 +0530

    Concurrent Hash Map

--- CollectionMastering/src/HashMap/Hashh.java
+++ CollectionMastering/src/HashMap/Hashh.java
@@ -1,6 +1,7 @@
 package HashMap;
 
 import java.util.Hashtable;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Hashh {
 
@@ -25,6 +26,27 @@ public class Hashh {
 
 
          */
+
+
+        ConcurrentHashMap<Integer,String> map2 = new ConcurrentHashMap<>();
+
+        /*
+
+        In JAVA 7 :
+            segment based locking ---> 16 segment by default ---> smallHashmaps
+            Only the segment to be written is being read
+
+        IN JAVA 8:
+            No segmentation .
+            We use Compare and swap
+            no locking except resizing and collision
+            Thread A last saw 45
+            Thread A set x -> 50
+            If X is still 45 then change to 50 or else don't change retry
+
+
+         */
+
 
     }
 
