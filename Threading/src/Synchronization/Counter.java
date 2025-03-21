f17ab2ba1c1b20241a3f99d18e16dedc776cf953
Shashank S <shashank5418shashu@gmail.com>
2025-03-21 11:05:56 +0530

    Used Synchronized and some learnt things about this

--- Threading/src/Synchronization/Counter.java
+++ Threading/src/Synchronization/Counter.java
@@ -4,7 +4,7 @@ public class Counter {
 
     private int count;
 
-    public void increment(){
+    public synchronized void increment(){
         count++;
     }
 
@@ -13,3 +13,31 @@ public class Counter {
     }
 
 }
+
+/*
+
+    public synchronized void increment(){
+        count++;
+    }
+
+    This is the way to make method synchronized
+    we can even make a part of a block synchronized
+    like this
+
+    public void increment(){
+        synchronized{
+            count++
+        }
+    }
+
+    if we use synchronized key word and two thread are sharing a single resource it will allow only one thread to take
+    run at a time hence getting the desired output
+
+    If we don't use the synchronized in that case it will end up in race condition.
+
+    The section were we are using synchronized keyword is called critical condition.
+
+
+
+
+ */
