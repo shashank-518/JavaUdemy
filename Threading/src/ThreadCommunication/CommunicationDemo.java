78f907c4fa06c4894a620da22b812a8c99b16d35
Shashank S <shashank5418shashu@gmail.com>
2025-03-23 23:23:37 +0530

    Code setup

--- Threading/src/ThreadCommunication/CommunicationDemo.java
+++ Threading/src/ThreadCommunication/CommunicationDemo.java
@@ -11,10 +11,45 @@ class SharedResource{
 
     }
 
-    public void consume(){
+    public int consume(){
 
     }
 
+}
+
+class Producer implements Runnable{
+
+    private SharedResource resource;
+
+    public Producer(SharedResource resource){
+        this.resource = resource;
+    }
+
+    @Override
+    public void run() {
+        for (int i =0;i<10;i++){
+            resource.produce(i);
+            System.out.println("Produced " + i);
+        }
+    }
+}
+
+
+class Consumer implements Runnable{
+
+    private SharedResource resource;
+
+    public Consumer(SharedResource resource){
+        this.resource = resource;
+    }
+
+    @Override
+    public void run() {
+        for (int i =0;i<10;i++){
+            int value = resource.consume();
+            System.out.println("Produced " + value);
+        }
+    }
 }
 
 public class CommunicationDemo {
