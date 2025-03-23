2e22f6fbaf475d95a088cc39e9d6ba6e48ae2ed2
Shashank S <shashank5418shashu@gmail.com>
2025-03-23 23:14:24 +0530

    Learning thread Communication

---
+++ Threading/src/ThreadCommunication/CommunicationDemo.java
@@ -0,0 +1,21 @@
+package ThreadCommunication;
+
+
+class SharedResource{
+
+    private int data;
+
+    private boolean hasData;
+
+    public void produce(int data){
+
+    }
+
+    public void consume(){
+
+    }
+
+}
+
+public class CommunicationDemo {
+}
